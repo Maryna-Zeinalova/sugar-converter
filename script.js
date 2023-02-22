119d411e579a33a31fecf2618145f1fdbac547ba
Maryna Zeinalova <shmapetto@gmail.com>
2023-02-22 21:42:10 +0200

    edited functions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let result = document.querySelector(".output-field");
-function count() {
-  let amount = document.getElementById("amount").value;
-  let glass = amount/200;
-  let tablespoon = amount/20;
-  let teaspoon = amount/8;
+function countSugar(gram) {
+  return {
+    glass: gram / 200,
+    tablespoon: gram / 20,
+    teaspoon: gram / 8,
+  };
+}
 
-  result.innerHTML = `
-  <h2 class="subtitle">${amount} грамів цукру =</h2>
+function renderCount() {
+  const initialAmount = document.getElementById("amount").value;
+  const resultAmount = countSugar(initialAmount);
+  document.querySelector(".output-field").innerHTML = `
+  <h2 class="subtitle">${initialAmount} грамів цукру =</h2>
   <ul>
-    <li><img class="measure-item" src="./images/glass.png" alt="glass" />${glass} стаканів</li>
-    <li><img class="measure-item" src="./images/tablespoon.png" alt="tablespoon" />${tablespoon} столових ложок</li>
-    <li><img class="measure-item" src="./images/teaspoon.png" alt="teaspoon" />${teaspoon} чайних ложок</li>
+    <li><img class="measure-item" src="./images/glass.png" alt="glass" />${resultAmount.glass} стаканів</li>
+    <li><img class="measure-item" src="./images/tablespoon.png" alt="tablespoon" />${resultAmount.tablespoon} столових ложок</li>
+    <li><img class="measure-item" src="./images/teaspoon.png" alt="teaspoon" />${resultAmount.teaspoon} чайних ложок</li>
   </ul>`;
 }
